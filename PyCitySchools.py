e4cebab57cde5fc8a8dc4a46f8902f43815b115e
Seghen Haile <shaile@credence-llc.com>
2019-12-14 16:39:06 -0500

    school summary complete.

--- PyCitySchools.py
+++ PyCitySchools.py
@@ -304,3 +304,98 @@ district_summary_df = district_summary_df[new_column_order]
 district_summary_df
 
 # %%
+# Determine the school type.
+per_school_types = school_data_df.set_index(["school_name"])["type"]
+per_school_types
+
+# %%
+# Add the per_school_types into a DataFrame for testing.
+df = pd.DataFrame(per_school_types)
+df
+
+# %%
+# Calculate the total student count.
+per_school_counts = school_data_df["size"]
+per_school_counts
+
+# %%
+school_data_complete_df
+
+# %%
+# Calculate the total student count.
+per_school_counts = school_data_complete_df["school_name"].value_counts()
+per_school_counts
+
+# %%
+# Calculate the total school budget.
+per_school_budget = school_data_df.set_index(["school_name"])["budget"]
+per_school_budget
+
+# %%
+# Calculate the per capita spending.
+per_school_capita = per_school_budget / per_school_counts
+per_school_capita
+
+# %%
+# Calculate the math scores.
+student_school_name = student_data_df.set_index(["school_name"])["math_score"]
+student_school_name
+# %%
+# Calculate the average math scores.
+per_school_averages = school_data_complete_df.groupby(["school_name"]).mean()
+per_school_averages
+
+# %%
+# Calculate the average test scores.
+per_school_math = school_data_complete_df.groupby(["school_name"]).mean()["math_score"]
+
+per_school_reading = school_data_complete_df.groupby(["school_name"]).mean()["reading_score"]
+
+# %%
+# Calculate the passing scores by creating a filtered DataFrame.
+per_school_passing_math = school_data_complete_df[(school_data_complete_df["math_score"] >= 70)]
+
+per_school_passing_reading = school_data_complete_df[(school_data_complete_df["reading_score"] >= 70)]
+
+# %%
+# Calculate the number of students passing math and passing reading by school.
+per_school_passing_math = per_school_passing_math.groupby(["school_name"]).count()["student_name"]
+
+per_school_passing_reading = per_school_passing_reading.groupby(["school_name"]).count()["student_name"]
+
+# %%
+# Calculate the percentage of passing math and reading scores per school.
+per_school_passing_math = per_school_passing_math / per_school_counts * 100
+
+per_school_passing_reading = per_school_passing_reading / per_school_counts * 100
+
+# %%
+# Calculate the overall passing percentage.
+per_overall_passing_percentage = (per_school_passing_math + per_school_passing_reading ) / 2
+
+# %%
+# Adding a list of values with keys to create a new DataFrame.
+
+per_school_summary_df = pd.DataFrame({
+             "School Type": per_school_types,
+             "Total Students": per_school_counts,
+             "Total School Budget": per_school_budget,
+             "Per Student Budget": per_school_capita,
+             "Average Math Score": per_school_math,
+           "Average Reading Score": per_school_reading,
+           "% Passing Math": per_school_passing_math,
+           "% Passing Reading": per_school_passing_reading,
+           "% Overall Passing": per_overall_passing_percentage})
+per_school_summary_df.head()
+
+# %%
+# Format the Total School Budget and the Per Student Budget columns.
+per_school_summary_df["Total School Budget"] = per_school_summary_df["Total School Budget"].map("${:,.2f}".format)
+
+per_school_summary_df["Per Student Budget"] = per_school_summary_df["Per Student Budget"].map("${:,.2f}".format)
+
+
+# Display the data frame
+per_school_summary_df.head()
+
+# %%
