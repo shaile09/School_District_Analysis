c51bb0007d6a9618693910a1e892f37f40903cd1
Seghen Haile <shaile@credence-llc.com>
2019-12-14 17:57:22 -0500

    scores by school type

--- PyCitySchools.py
+++ PyCitySchools.py
@@ -631,3 +631,39 @@ size_summary_df["% Overall Passing"] = size_summary_df["% Overall Passing"].map(
 size_summary_df 
 
 # %%
+# Calculate averages for the desired columns. 
+type_math_scores = per_school_summary_df.groupby(["School Type"]).mean()["Average Math Score"]
+
+type_reading_scores = per_school_summary_df.groupby(["School Type"]).mean()["Average Reading Score"]
+
+type_passing_math = per_school_summary_df.groupby(["School Type"]).mean()["% Passing Math"]
+
+type_passing_reading = per_school_summary_df.groupby(["School Type"]).mean()["% Passing Reading"]
+
+type_overall_passing = (type_passing_math + type_passing_reading) / 2
+
+# %%
+# Assemble into DataFrame.
+type_summary_df = pd.DataFrame({
+          "Average Math Score" : type_math_scores,
+          "Average Reading Score": type_reading_scores,
+          "% Passing Math": type_passing_math,
+          "% Passing Reading": type_passing_reading,
+          "% Overall Passing": type_overall_passing})
+
+type_summary_df
+# %%
+# Formatting
+type_summary_df["Average Math Score"] = type_summary_df["Average Math Score"].map("{:.1f}".format)
+
+type_summary_df["Average Reading Score"] = type_summary_df["Average Reading Score"].map("{:.1f}".format)
+
+type_summary_df["% Passing Math"] = type_summary_df["% Passing Math"].map("{:.0f}".format)
+
+type_summary_df["% Passing Reading"] = type_summary_df["% Passing Reading"].map("{:.0f}".format)
+
+type_summary_df["% Overall Passing"] = type_summary_df["% Overall Passing"].map("{:.0f}".format)
+
+type_summary_df
+
+# %%
